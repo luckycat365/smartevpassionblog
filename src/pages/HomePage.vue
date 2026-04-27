4902c5899988e42a3c838581720e70f1908b6bbf
fusheng <shengzhe.fu@mercedes-benz.com>
2026-04-27 22:07:48 +0200

    feat: add HomePage with brand grid

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="grid-container">
+    <BrandCard v-for="brand in EVData" :key="brand.id" :brand="brand" />
+  </div>
+</template>
+
+<script setup>
+import { EVData } from '../data.js'
+import BrandCard from '../components/BrandCard.vue'
+</script>
